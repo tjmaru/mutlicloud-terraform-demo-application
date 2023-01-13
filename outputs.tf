34b44697faa5ead221b71a0f71dc56efe2d60d56
tjmaru <30507792+tjmaru@users.noreply.github.com>
2023-01-13 16:11:37 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,6 @@
 output "public_ip_address" {
   description = "The actual ip address allocated for the resource."
   value = coalesce(
-    join("", module.aws_app.public_ip),
-    join("", data.azurerm_public_ip.vm.*.ip_address),
     local.gcp ? google_compute_instance.vm.0.network_interface.0.access_config.0.nat_ip : "dummy"
   )
 }
